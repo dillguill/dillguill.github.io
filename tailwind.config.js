db7992a1ecd34d3b06d5b8f84d18adbaba94bafb
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-19 19:35:52 -0500

    Changed global font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "beige": "#dec4ad", 
-        "space-gray": "#1c1c1e", 
-        'sunset-orange': '#ff5e3a'
+        beige: "#dec4ad",
+        "space-gray": "#1c1c1e",
+        "sunset-orange": "#ff5e3a",
+        "neon-blue": "#00f6ff", 
+        "galaxy-purple": "#8000ff",
+        "space-gray": "#1c1c1e",
       },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],  // For headers
+        mono: ['Space Mono', 'monospace'],   // For paragraphs and body      },
+      }
     },
   },
   plugins: [],
