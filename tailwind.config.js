8737b3981bc4ed352bde551fd2a4f3c8a3cf730a
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-23 17:48:26 -0500

    Changed fonts & icons; Added dark mode functionality to blob

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        spaceGrotesk: ['Space Grotesk', 'sans-serif'], // Custom heading font
+        spaceMono: ['Space Mono', 'monospace'],        // Custom body font
+      },
     },
   },
   plugins: [],
