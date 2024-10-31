d66005dc05d04ee61051ba636566fa7dc9f43e48
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-30 23:54:32 -0500

    Updated text

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -16,7 +16,7 @@ export default function Example() {
     <div className="relative isolate overflow-hidden py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-5xl font-semibold tracking-tight sm:text-7xl">Work with us</h2>
+          <h2 className="text-5xl font-semibold tracking-tight sm:text-7xl">About me</h2>
           <p className="mt-8 text-pretty text-lg font-medium sm:text-xl/8">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
             fugiat veniam occaecat fugiat.
--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -24,8 +24,8 @@ export default function Example() {
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">From the blog</h2>
-          <p className="mt-2 text-lg/8">Learn how to grow your business with our expert advice.</p>
+          <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">Projects</h2>
+          <p className="mt-2 text-lg/8">Anim aute id magna aliqua ad ad non deserunt sunt.</p>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
