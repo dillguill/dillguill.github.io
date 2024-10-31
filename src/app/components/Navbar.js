9a94468172008f0877f524e16f6f6c1c98965e95
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-30 23:04:55 -0500

    Updated navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,17 +1,12 @@
-import { HomeIcon, UserIcon, BriefcaseIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import { Bars2Icon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-30 flex justify-between p-4 md:p-6 bg-opacity-40 backdrop-blur-lg">
   <div className="text-xl md:text-2xl font-bold">MyPortfolio</div>
-  <ul className="flex space-x-4 md:space-x-6 text-sm md:text-base">
-    <li className="flex items-center space-x-1">
-      <HomeIcon className="h-4 w-4 md:h-5 md:w-5" />
-      <a href="#home" className="hover:underline">Home</a>
-    </li>
-    <li className="flex items-center space-x-1">
-      <UserIcon className="h-4 w-4 md:h-5 md:w-5" />
-      <a href="#about" className="hover:underline">About</a>
+  <ul className="">
+    <li className="">
+      <Bars2Icon className="h-10 w-10 md:h-15 md:w-15" />
     </li>
   </ul>
 </nav>
