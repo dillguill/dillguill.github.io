eef1dcf26e0b25eb18eaeb387b8f0f186f359ee6
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-30 23:58:19 -0500

    Build issue fix

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -159,7 +159,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-stone-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Let&#39;s talk
           </button>
         </div>
       </form>
