55da8e2c0a251dee92590796648140b84bae4a25
Dillon <162556809+dillguill@users.noreply.github.com>
2024-10-31 23:40:06 -0500

    Fixed logo resize

--- src/app/components/LogoCloud.js
+++ src/app/components/LogoCloud.js
@@ -2,7 +2,7 @@ export default function Example() {
   return (
     <div className="py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 grid-rows-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-3 grid-rows-2 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           {/* <img
               alt="HTML"
               src="https://icons8.com/icon/23028/html-5"
