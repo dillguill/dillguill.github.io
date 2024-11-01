094687d8844a402a0bed6b45fc5640b364bee77c
Dillon <162556809+dillguill@users.noreply.github.com>
2024-11-01 18:35:25 -0500

    Updated colors

--- src/app/components/Projects.js
+++ src/app/components/Projects.js
@@ -27,28 +27,28 @@ export default function Example() {
           <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">Projects</h2>
           <p className="mt-2 text-lg/8">Anim aute id magna aliqua ad ad non deserunt sunt.</p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-stone-800 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
             <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
               <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="">
+                <time dateTime={post.datetime} className="text-stone-200">
                   {post.date}
                 </time>
                 <a
                   href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium hover:bg-gray-100"
+                  className="relative z-10 rounded-full border-none bg-white/15 px-3 py-1.5 font-medium hover:bg-gray-100"
                 >
                   {post.category.title}
                 </a>
               </div>
               <div className="group relative">
-                <h3 className="mt-3 text-lg/6 font-semibold group-hover:text-gray-600">
+                <h3 className="mt-3 text-lg/6 font-semibold group-hover:text-stone-200">
                   <a href={post.href}>
                     <span className="absolute inset-0" />
                     {post.title}
                   </a>
                 </h3>
-                <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{post.description}</p>
+                <p className="mt-5 line-clamp-3 text-sm/6">{post.description}</p>
               </div>
               <div className="relative mt-8 flex items-center gap-x-4">
                 <img alt="" src={post.author.imageUrl} className="h-10 w-10 rounded-full bg-gray-50" />
@@ -59,7 +59,7 @@ export default function Example() {
                       {post.author.name}
                     </a>
                   </p>
-                  <p className="text-gray-600">{post.author.role}</p>
+                  <p className="text-stone-200">{post.author.role}</p>
                 </div>
               </div>
             </article>
