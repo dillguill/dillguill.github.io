61a0c1c3a72fffa81cbfcca7b5325a7747d4eb70
Dillon <162556809+dillguill@users.noreply.github.com>
2025-01-19 18:23:17 -0600

    Updated Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { NavbarSimple } from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
@@ -12,7 +12,7 @@ import Contact from './components/Contact';
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavbarSimple />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
