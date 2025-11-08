2ce2dfa42a8741241cc97d3f9f1316081c992a42
Dillon <162556809+dillguill@users.noreply.github.com>
2025-11-08 16:53:28 -0600

    Removed unused components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavbarSimple } from './components/Navbar';
 import Header from './components/Header';
-// import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
-// import Resume from './components/Resume';
 import Projects from './components/Projects';
 import SocialLinks from './components/SocialLinks';
-// import Contact from './components/Contact';
 
 function App() {
   return (
@@ -18,10 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        {/* <Route path="/resume" element={<Resume />} /> */}
         <Route path="/projects" element={<Projects />} />
         <Route path="/socials" element={<SocialLinks />} />
-        {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
       <NavbarSimple />
       {/* <Footer /> */}
