3986f343af64ab23d4e4f1cd410cc5582961fd9f
Dillon <162556809+dillguill@users.noreply.github.com>
2025-11-08 22:26:20 -0600

    Fixes with gh-pages deployment

--- next.config.js
+++ next.config.js
@@ -5,12 +5,16 @@ const withMDX = require('@next/mdx')({
   },
 })
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  // Enable static export for GitHub Pages
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Add basePath and assetPrefix for GitHub Pages
+  basePath: isProd ? '' : '',
+  assetPrefix: isProd ? '' : '',
 })
