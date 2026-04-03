6451c12b4b3f5ba20b26a74a795a6c0f40829a55
Dillon <162556809+dillguill@users.noreply.github.com>
2026-04-02 20:53:45 -0500

    Removed reference to SocialLinks

--- pages/socials.js
+++ pages/socials.js
@@ -1,5 +1,4 @@
 import Header from '../src/components/Header';
-import SocialLinks from '../src/components/SocialLinks';
 import { NavbarSimple } from '../src/components/Navbar';
 
 export default function SocialsPage() {
